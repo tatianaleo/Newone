9251b280961cd3a4d344a096420cf7a918c0b50e
Tatiana <kudrina_tatyana@mail.ru>
2023-04-11 11:45:41 +0200

    Quinto archivo

---
+++ Example05/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Insert the username");
+string username = Console.ReadLine();
+if (username.ToLower() ==  "masha")
+{
+    Console.WriteLine("Wow!Its MAsha");
+}
+else{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
